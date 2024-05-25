b71b7af961406bc78ee4a8f1fc60fa196fe14b39
datikuknishvili <kuknishvilidati@gmail.com>
2024-05-25 22:51:48 +0400

    commit

---
+++ day3/homework/homework3.py
@@ -0,0 +1,17 @@
+print(5 + 5)
+print(5-5)
+print(20/10)
+print(5*10)
+
+print("bevri" + "manqana")
+#print funqcis dawerit ori sityva ertmanets shevuertet.
+
+
+
+print((10 + 10 + 30) * 5)
+print((100/10 + 50) / 4)
+
+
+#სტრინგი არის ტექსტური მონაცემები, მთელი რიცხვი იგივე ინტეგერი არის რიცხვები.
+
+
